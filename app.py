20134de9e5bf77da585f1f9c7a86f6e905fe95c4
Chin Yan Keat <chinyankeat@yahoo.com>
2017-04-05 00:32:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    baseurl = "https://xml.weather.yahoo.com/v1/public/yql?"
     yql_query = makeYqlQuery(req)
     if yql_query is None:
         return {}
